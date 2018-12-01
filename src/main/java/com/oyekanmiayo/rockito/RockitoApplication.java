f84480f82ce9284f8686426ea544830160668ffa
Ayomide Oyekanmi <aoyekanmi@teamapt.com>
2018-12-01 22:51:50 +0100

    Initialize spring boot application

---
+++ src/main/java/com/oyekanmiayo/rockito/RockitoApplication.java
@@ -0,0 +1,12 @@
+package com.oyekanmiayo.rockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockitoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oyekanmiayo/rockito/RockitoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oyekanmiayo.rockito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RockitoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
