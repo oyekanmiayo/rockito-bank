8574ece85abfc832f3ee95582c8462012d42ffa7
Ayomide Oyekanmi <aoyekanmi@teamapt.com>
2018-12-01 23:21:05 +0100

    feat: Add entity models

---
+++ src/main/java/com/oyekanmiayo/rockito/domain/SavingsTransaction.java
@@ -0,0 +1,98 @@
+package com.oyekanmiayo.rockito.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class SavingsTransaction {
+    private long id;
+
+    private Date date;
+
+    private String description;
+
+    private String type;
+
+    private String status;
+
+    private Double amount;
+
+    private BigDecimal availableBalance;
+
+    private SavingsAccount savingsAccount;
+
+    public SavingsTransaction(){ }
+
+    public SavingsTransaction(Date date, String description, String type, String status, Double amount, BigDecimal availableBalance, SavingsAccount savingsAccount) {
+        this.date = date;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+        this.amount = amount;
+        this.availableBalance = availableBalance;
+        this.savingsAccount = savingsAccount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public void setAvailableBalance(BigDecimal availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+
+    public SavingsAccount getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(SavingsAccount savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
+}
---
+++ src/main/java/com/oyekanmiayo/rockito/domain/User.java
@@ -0,0 +1,127 @@
+package com.oyekanmiayo.rockito.domain;
+
+import java.util.List;
+
+public class User  {
+    private long userId;
+
+    private String username;
+
+    private String password;
+
+    private  String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String phone;
+
+    private boolean enabled = true;
+
+    private PrimaryAccount primaryAccount;
+
+    private SavingsAccount savingsAccount;
+
+    private List<Appointment> appointmentList;
+
+    private List<Recipient> recipientList;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public PrimaryAccount getPrimaryAccount() {
+        return primaryAccount;
+    }
+
+    public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+        this.primaryAccount = primaryAccount;
+    }
+
+    public SavingsAccount getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(SavingsAccount savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", enabled=" + enabled +
+                ", primaryAccount=" + primaryAccount +
+                ", savingsAccount=" + savingsAccount +
+                ", appointmentList=" + appointmentList +
+                ", recipientList=" + recipientList +
+                '}';
+    }
+}
